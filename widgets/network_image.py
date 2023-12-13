0ddc565a5700a60fc4eb557d431d7863a590f9fb
kengenal <kengenal1@gmail.com>
2023-12-13 12:19:34 +0100

    Chage class to fetch images

--- widgets/network_image.py
+++ widgets/network_image.py
@@ -1,20 +1,29 @@
-import gi
+import threading
 
-from gi.repository import Gtk, GdkPixbuf, GLib, Gio
+import requests
+from gi.repository import GdkPixbuf, Gio, GLib, Gtk
 
 
 class AsyncImage(Gtk.Image):
     def __init__(self, url: str, *args, **kwargs):
-        super().__init__( *args, **kwargs)
-        file = Gio.file_new_for_uri(url)
-    
-        file.load_contents_async(None, self.on_load_complete)
+        super().__init__(*args, **kwargs)
+        self.url = url
+        threading.Thread(target=self.on_load, daemon=True).start()
 
     def on_load_complete(self, file: Gio.File, result: Gio.AsyncResult):
         is_loaded, stream, _ = file.load_contents_finish(result)
         if is_loaded:
             loader = GdkPixbuf.PixbufLoader()
             loader.write_bytes(GLib.Bytes.new(stream))
+            loader.close()
+
+            self.set_from_pixbuf(loader.get_pixbuf())
+
+    def on_load(self):
+        result = requests.get(self.url)
+        if result.status_code == 200:
+            loader = GdkPixbuf.PixbufLoader()
+            loader.write_bytes(GLib.Bytes.new(result.content))
             loader.close()
 
             self.set_from_pixbuf(loader.get_pixbuf())
